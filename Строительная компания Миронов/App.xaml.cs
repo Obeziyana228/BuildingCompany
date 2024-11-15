5ea99bcfe5287ed2e81d545ce3849b12507a2da3
222214 <222214@B42E99861EB11>
2024-11-15 18:49:59 +0300

    Добавьте файлы проекта.

---
+++ Строительная компания Миронов/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Строительная_компания_Миронов
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
